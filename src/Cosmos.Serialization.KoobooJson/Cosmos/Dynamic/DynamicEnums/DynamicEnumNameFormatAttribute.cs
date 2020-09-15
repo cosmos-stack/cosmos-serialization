547b482354efe8b622576cd3a8b1d93cf969c7b2
alexinea <forerunner@outlook.com>
2020-09-15 23:59:56 +0800

    Add DynamicEnums support.

---
+++ src/Cosmos.Serialization.KoobooJson/Cosmos/Dynamic/DynamicEnums/DynamicEnumNameFormatAttribute.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Reflection;
+using Cosmos.Reflection;
+using Cosmos.Text;
+using Kooboo.Json;
+
+namespace Cosmos.Dynamic.DynamicEnums
+{
+    public class DynamicEnumNameFormatAttribute : ValueFormatAttribute
+    {
+        public override string WriteValueFormat(object value, Type type, JsonSerializerHandler handler, out bool isValueFormat)
+        {
+            isValueFormat = true;
+
+            if (Enums.IsDynamicEnum(type, out _))
+            {
+                var val = value.GetPropertyValue("Name");
+
+                if (val is string stringVal)
+                    return stringVal;
+
+                return null;
+            }
+
+            return null;
+        }
+
+        public override object ReadValueFormat(string value, Type type, JsonDeserializeHandler handler, out bool isValueFormat)
+        {
+            isValueFormat = true;
+
+            if (Enums.IsDynamicEnum(type, out var genericArguments))
+            {
+                if (value is null || string.IsNullOrEmpty(value))
+                    return default;
+
+                var targetType = typeof(DynamicEnum<,>).MakeGenericType(genericArguments);
+                var methodInfo = targetType.GetMethod("FromName")!;
+                return methodInfo!.Invoke(null, new object[] {value});
+            }
+
+            return default;
+        }
+    }
+}
---
+++ src/Cosmos.Serialization.SwifterJson/Cosmos/Dynamic/DynamicEnums/DynamicEnumNameInterface.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Swifter.RW;
+
+namespace Cosmos.Dynamic.DynamicEnums
+{
+    public class DynamicEnumNameInterface<TEnum, TValue> : IValueInterface<TEnum>
+        where TEnum : DynamicEnum<TEnum, TValue>, IDynamicEnum
+        where TValue : struct, IEquatable<TValue>, IComparable<TValue>
+    {
+        public TEnum ReadValue(IValueReader valueReader)
+        {
+            var name = valueReader.ReadString();
+            return DynamicEnum<TEnum, TValue>.FromName(name);
+        }
+
+        public void WriteValue(IValueWriter valueWriter, TEnum value)
+        {
+            if (value is null)
+                valueWriter.DirectWrite(null);
+            else
+                valueWriter.WriteString(value.Name);
+        }
+    }
+}
